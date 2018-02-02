2ab1d3340cf557c784b3e584b057aafb9452d41e
Narisu.Tao <narisu.tao@eon.com>
2018-02-02 15:40:07 +0100

    Transform the python 2 code into python 3

--- spearmint/examples/rosenbrocks_valley/rosenbrock.py
+++ spearmint/examples/rosenbrocks_valley/rosenbrock.py
@@ -4,7 +4,7 @@ def rosenbrocks_valley(xs):
     sum = 0
     last_x = xs[0]
 
-    for i in xrange(1, len(xs)):
+    for i in range(1, len(xs)):
         sum += (100 * math.pow((xs[i] - math.pow(last_x, 2)), 2)) + math.pow(1 - last_x, 2)
 
     return sum
@@ -13,10 +13,10 @@ def main(job_id, params):
   xs = params['X']
   res = rosenbrocks_valley(xs)
 
-  print "Rosenbrock's Valley in %d dimensions" % (len(xs))
-  print "\tf(",
-  print xs,
-  print ") = %f" % (res)
+  print("Rosenbrock's Valley in %d dimensions" % (len(xs)))
+  print("\tf(", end=' ')
+  print(xs, end=' ')
+  print(") = %f" % (res))
 
   return rosenbrocks_valley(xs)
 
